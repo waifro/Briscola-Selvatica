bc228cfaa51fb94d551d090a6d60af6595b1181c
waifro <waifro.bio@gmail.com>
2022-02-22 18:15:11 +0500

    Co-authored-by: Simomerlo10 <Simomerlo10@users.noreply.github.com>

---
+++ main.js
@@ -0,0 +1,14 @@
+
+// group
+var foo = "cazzo guardi";
+alert("ciao " + foo);
+
+// per comporre i array si usa questa sintasse:
+// var variabile = [ "array 1", "array 2" , "etc" ];
+
+// 4 gruppi di cui conterranno 10 carte e le proprie caratteristiche
+var oggetto_spade = null;
+var oggetto_denari = null;
+var oggetto_bastoni = null;
+var oggetto_coppe = null;
+
