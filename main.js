024daf36ea64a3c3629c5efd33b85c7fbc60a494
waifro <waifro.bio@gmail.com>
2022-02-23 12:51:54 +0500

    test

--- main.js
+++ main.js
@@ -24,12 +24,19 @@ function RenderImage(url, x, y, w, h, funct) {
     return image;
 }
 
+var window_x = 100;
+var window_y = 50;
+
 function main_init() {
     
+    document.getElementById("bho").write = "idk";
+
     console.log("window.x " + innerWidth);
     console.log("window.h " + innerHeight);
 
-    RenderImage("resources/images/test.png", 50, 50, 50, 50);
+    for (var x = 0; x < window_x; x+=10) {
+        RenderImage("resources/images/test.png", x, 50, 50, 50);
+    }
 
 }
 
