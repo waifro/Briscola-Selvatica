e1bde757e4290ccb7c0d520ca626bbd0ed6bb70b
waifro <waifro.bio@gmail.com>
2022-02-28 09:47:13 +0500

    testing

--- javascript/gioco.js
+++ javascript/gioco.js
@@ -9,15 +9,17 @@ function raw_carta() {
 }
 
 function raw_imagesize(pathname) {
+    this.w = 0;
+    this.h = 0;
 
     let img = new Image();
     img.src = pathname;
 
     img.onload = function() {
-        this.width = img.width;
-        this.height = img.height;
-        console.log("w: " + this.width);
-        console.log("h: " + this.height);
+        this.w = img.width;
+        this.h = img.height;
+        console.log("w: " + this.w);
+        console.log("h: " + this.h);
     }
 
     return this;
@@ -27,8 +29,8 @@ function inizializza_mazzo(pathname) {
 
     var size = raw_imagesize(pathname);
     
-    alert(size.width + "x" + size.height);
-    
+    if (size.w == 0 && size.h == 0) return;
+
     var array = [];
 
     var foo = size.width / 10;
@@ -42,14 +44,14 @@ function inizializza_mazzo(pathname) {
 
         buffer.x = i;
         buffer.y = o;
-        buffer.w = size.width;
-        buffer.h = size.height;
+        buffer.w = size.w;
+        buffer.h = size.h;
 
         array[n] = buffer;
 
-        i += size.width;
+        i += size.w;
         if (n == 10 || n == 20 || n == 30) {
-            o += size.height;
+            o += size.h;
             alert(buffer.x + "x" + buffer.y + " wh: " + buffer.w + "x" + buffer.h);
         }
     }
@@ -61,7 +63,7 @@ function gioco_inizio() {
 
     var mazzo = inizializza_mazzo("risorse/img/pc_carte.jpg");
 
-    canvas_RAW_GeneraFiguraRitaglio("risorse/img/pc_carte.jpg", mazzo[1].x, mazzo[1].y, mazzo[1].w, mazzo[1].h, 0, 0, mazzo[1].w, mazzo[1].h);
+    canvas_RAW_GeneraFigura("risorse/img/poker_background.jpg", 0, 0, canvas.width, canvas.height);
 
     //alert(mazzo[1].x + "x" + mazzo[1].y + " wh: " + mazzo[1].w + "x" + mazzo[1].h);
     console.log("quantità mazzo: " + mazzo.length);
